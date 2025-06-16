1d758d1b02d955daf28918c7086bc0eaa5ac0e14
Kellan St.Louis <kstlouis@me.com>
2025-06-16 16:45:40 -0600

    remove tenant connection requirement; refactor env var export method

--- netskope-cli-tls-fix.sh
+++ netskope-cli-tls-fix.sh
@@ -1,76 +1,119 @@
-#!/bin/bash
+#!/bin/zsh
 
 ## This tool will try to detect common cli tools and will configure the Netskope SSL certificate bundle.
 
 # Get the current console user
+currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+echo "Current logged-in user: $currentUser"
 
-CURRENT_USER=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
-echo "Current logged-in user: $CURRENT_USER"
+ZSHRC="/Users/$currentUser/.zshrc"
+BASHRC="/Users/$currentUser/.bashrc"
+mkdir -p "/Users/$currentUser/.netskope" # Create the folder if not there
+finalCertPath="/Users/$currentUser/.netskope/netskope-CLI-cert-bundle.pem"
+netskopeCertPath="/Users/$currentUser/.netskope/nscacert_combined.pem"
+certFolderPath=$(dirname "$finalCertPath")
+certFileName=$(basename "$finalCertPath")
+awsHomeBrew="/opt/homebrew/bin/aws" # HomeBrew install
+awsRelease="/usr/local/bin/aws" # AWS Official Release 
+BLOCK_BEGIN="# >>> NETSKOPE CA BUNDLE ENV VARS >>>"
+BLOCK_END="# <<< NETSKOPE CA BUNDLE ENV VARS <<<"
 
-get_shell(){
-    # Get current user
-    CURRENT_USER=$(stat -f "%Su" /dev/console)
-    
-    # Get that user's shell from the system directory service
-    USER_SHELL=$(dscl . -read /Users/"$CURRENT_USER" UserShell | awk '{print $2}')
-    echo "Shell used is $USER_SHELL"
-    
-    if [[ $USER_SHELL == *"bash"* ]]; then
-        shell="/Users/$CURRENT_USER/.bash_profile"
-    else
-        shell="/Users/$CURRENT_USER/.zshenv"
-    fi
-    echo "Config file: $shell"
 
-    # Ensure the shell config file exists before using it
-    if [[ ! -f "$shell" ]]; then
-    echo "# Created by get_shell script" > "$shell"
-    fi
-}
-get_shell
-
-mkdir -p /Users/Shared/Netskope
-tenantName=$4
-orgKey=$5
-certDir="/Users/Shared/Netskope"
-certName="nscacert_combined.pem"
-
-status_code=$(curl -k --write-out %{http_code} --silent --output /dev/null https://$tenantName/locallogin)
-
-if [[ "$status_code" -ne "307" ]] ; then
-  echo "Tenant Unreachable"
-  exit 1
+# What AWS should we use.
+if [[ -x "$awsHomeBrew" ]]; then
+    awsBinary="$awsHomeBrew"
+    awsCertPath="/opt/homebrew/etc/ca-certificates/cert.pem"
+elif [[ -x "$awsRelease" ]]; then
+    awsBinary="$awsRelease"
+    awsCertPath="/usr/local/bin/aws/awscli/botocore/cacert.pem"
 else
-  echo "Tenant Reachable"
+    echo "AWS CLI not found. Skipping adding the AWS Cert Bundle"
+    awsCertPath=""
 fi
 
-# Function to create or update certificate bundle
-create_cert_bundle() {
-  echo "Creating cert bundle"
-  curl -k "https://addon-$tenantName/config/ca/cert?orgkey=$orgKey" > $certDir/$certName
-  curl -k "https://addon-$tenantName/config/org/cert?orgkey=$orgKey" >> $certDir/$certName
-  curl -k -L "https://ccadb-public.secure.force.com/mozilla/IncludedRootsPEMTxt?TrustBitsInclude=Websites" >> $certDir/$certName
-}
+# Create Bundle of all System Root Certs
+security find-certificate -a -p "/System/Library/Keychains/SystemRootCertificates.keychain" "/Library/Keychains/System.keychain" > "/tmp/nscacert_combined.pem"
+cp "/tmp/nscacert_combined.pem" "${certFolderPath}"
 
-if [ -f "$certDir/$certName" ]; then
-  echo "$certName already exists in $certDir."
-  read -p "Recreate Certificate Bundle? (y/N) " -n 1 -r
-  echo    
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    create_cert_bundle
-  fi
+if [[ -f "$netskopeCertPath" ]]; then
+  echo "Netskope Combined Cert was created"
 else
-  create_cert_bundle
+  echo "The Netskope Combined Cert was not Created. Exiting..."
+   # ErrorDialog=$($dialogPath "${dialogOptions[@]}" \
+     # --bannertitle "${holdingCompany} - Netskope CLI Bundle ERROR" \
+     # --message "The was Error creating the Netskope combined certificate bundle.<br><br>Please contant Support for help." \
+     # --button1text "Close" \
+     # --json )
+  exit 2
 fi
 
+# Create the Netskope and AWS Combined Bundle
+echo "Netskope Certs: $netskopeCertPath"
+if [ -f  "${awsCertPath}" ]; then
+  echo "AWS Certs: $awsCertPath"
+  echo "AWS CLI: $awsBinary"
+  cat "${awsCertPath}" "${netskopeCertPath}" > "${finalCertPath}"
+  echo "Netskope & AWS Combined Cert was created"
+else
+  cat "${netskopeCertPath}" > "${finalCertPath}"
+  echo "Netskope Combined Cert was created"
+fi 
 
+echo "Combined Cert: ${finalCertPath}"
+
+# Shell Settings to Add
+NEW_BLOCK=$(cat <<EOF
+$BLOCK_BEGIN
+export REQUESTS_CA_BUNDLE='${finalCertPath}' # Python/AZURE CLI/Other Tools         
+export AWS_CA_BUNDLE='${finalCertPath}' # AWS CLI
+export SSL_CERT_FILE='${finalCertPath}' # OPENSSL
+export NODE_EXTRA_CA_CERTS='${finalCertPath}' # NODE
+export CURL_CA_BUNDLE='${finalCertPath}' # CURL
+$BLOCK_END
+EOF
+)
+
+touch "$ZSHRC" # Ensure .zshrc exists
+touch "$BASHRC" # Ensure .BASHRC exists
+
+# Remove old block if it exists for ZSHRC
+if grep -q "$BLOCK_BEGIN" "$ZSHRC"; then
+    echo "Found existing Netskope CA bundle block in ${ZSHRC}. Removing it..."
+    # Use awk to remove the block between markers
+    awk "/$BLOCK_BEGIN/{flag=1;next}/$BLOCK_END/{flag=0;next}!flag" "$ZSHRC" > "${ZSHRC}.tmp" && mv "${ZSHRC}.tmp" "$ZSHRC"
+fi
+# Append new block to .zshrc
+echo -e "\n$NEW_BLOCK" >> "$ZSHRC"
+echo "Updated Netskope cert bundle exports in $ZSHRC"
+source "$HOME/.zshrc" # Load the ZShell File Now
+
+# Remove old block if it exists for BASHRC
+if grep -q "$BLOCK_BEGIN" "$BASHRC"; then
+    echo "Found existing Netskope CA bundle block in ${BASHRC}. Removing it..."
+    # Use awk to remove the block between markers
+    awk "/$BLOCK_BEGIN/{flag=1;next}/$BLOCK_END/{flag=0;next}!flag" "$BASHRC" > "${BASHRC}.tmp" && mv "${BASHRC}.tmp" "$BASHRC"
+fi
+# Append new block to .BASHRC
+echo -e "\n$NEW_BLOCK" >> "$BASHRC"
+echo "Updated Netskope cert bundle exports in $BASHRC"
+
+# Add Fix for older Bash Settings
+line='[ -f "$HOME/.bashrc" ] && . "$HOME/.bashrc"'
+touch "/Users/${currentUser}/.bash_profile" # Ensure .bash_profile exists
+grep -qxF "$line" "/Users/${currentUser}/.bash_profile" || echo -e "\n$line" >> "/Users/${currentUser}/.bash_profile"
+source "$HOME/.bashrc" # Load the Bash File Now
+
+export REQUESTS_CA_BUNDLE="${finalCertPath}" # Python/AZURE CLI/Other Tools         
+export AWS_CA_BUNDLE="${finalCertPath}" # AWS CLI
+export SSL_CERT_FILE="${finalCertPath}" # OPENSSL
+export NODE_EXTRA_CA_CERTS="${finalCertPath}" # NODE
+export CURL_CA_BUNDLE="${finalCertPath}" # CURL
 
 # Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Function to configure a tool with the certificate bundle
 configure_tool() {
   local tool_name=$1
   local env_var=$2
@@ -99,28 +142,16 @@ configure_tool() {
       echo "$post_command" >> configured_tools.sh
     fi
   else
-    echo "$tool_name is not installed"
+    echo "$tool_name is not installed, ignoring"
   fi
 }
 
-# This allows for later silent runs on other machines
-> configured_tools.sh
+configure_tool "NodeJS Package Manager (NPM)" "" "npm" "npm config set cafile ${finalCertPath}"
 
-# Configure tools
-configure_tool "gam" "" "/Users/$CURRENT_USER/bin/gamadv-xtd3/gam" "/Users/$CURRENT_USER/bin/gamadv-xtd3/gam config cacerts_pem $certDir/$certName save"
-configure_tool "Git" "GIT_SSL_CAPATH" "git" ""
-configure_tool "OpenSSL" "SSL_CERT_FILE" "openssl" ""
-configure_tool "cURL" "SSL_CERT_FILE" "curl" ""
-configure_tool "Python Requests Library" "REQUESTS_CA_BUNDLE" "" ""
-configure_tool "AWS CLI" "AWS_CA_BUNDLE" "awscli" ""
-configure_tool "Google Cloud CLI" "" "gcloud" "gcloud config set core/custom_ca_certs_file $certDir/$certName"
-configure_tool "NodeJS Package Manager (NPM)" "" "npm" "npm config set cafile $certDir/$certName"
-configure_tool "NodeJS" "NODE_EXTRA_CA_CERTS" "node" ""
-configure_tool "Ruby" "SSL_CERT_FILE" "ruby" ""
-configure_tool "PHP Composer" "" "composer" "composer config --global cafile $certDir/$certName"
-configure_tool "GoLang" "SSL_CERT_FILE" "go" ""
-configure_tool "Azure CLI" "REQUESTS_CA_BUNDLE" "az" ""
-configure_tool "Python PIP" "REQUESTS_CA_BUNDLE" "pip3" ""
-configure_tool "Oracle Cloud CLI" "REQUESTS_CA_BUNDLE" "oci-cli" ""
-configure_tool "Cargo Package Manager" "SSL_CERT_FILE" "cargo" ""
-configure_tool "Yarn" "" "yarnpkg" "yarnpkg config set httpsCaFilePath $certDir/$certName"
+## Clean Up
+if [[ -f "$netskopeCertPath" ]]; then
+  rm -f "$netskopeCertPath"
+fi
+if [[ -f "/tmp/nscacert_combined.pem" ]]; then
+  rm -f "/tmp/nscacert_combined.pem"
+fi
